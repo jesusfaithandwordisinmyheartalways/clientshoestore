0aa74b673bb31843700e99f0ba2ddb528bedca72
Andrew Johnson <90214404+jesusfaithandwordisinmyheartalways@users.noreply.github.com>
2025-03-07 12:09:25 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000'
+    origin: 'https://clientshoestoreclient.onrender.com'
 }))
 app.use(helmet());
 
